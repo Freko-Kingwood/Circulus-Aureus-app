c2f1625609641ad8c8503da16eb9c750a1426937
Freko-Kingwood <frekopetersen1998@gmail.com>
2026-03-26 18:46:23 +0100

    Delete netlify/functions directory

--- netlify/functions/identity-signup.mjs
+++
@@ -1,9 +0,0 @@
-export default async (req) => {
-  const { user } = await req.json();
-  return Response.json({
-    app_metadata: {
-      ...user.app_metadata,
-      roles: ['member']
-    }
-  });
-};
